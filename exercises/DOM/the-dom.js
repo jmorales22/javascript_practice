2255b71449c0a383f53ac9e4f09807447e5e9005
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-12-14 15:46:44 -0500

    Started the DOM module

---
+++ exercises/DOM/the-dom.js
@@ -0,0 +1,29 @@
+const p = document.querySelectorAll('p');
+console.log(p)
+
+const imgs = document.querySelector('.item img');
+console.log(imgs);
+
+const heading = document.querySelector('h2');
+//.dir shows us the object properties - they be used as getters or setters
+// console.dir(heading)
+
+// //example of a getter
+// console.dir(heading.textContent);
+
+// //example of a setter
+// heading.textContent = "This is the best!";
+
+//shows what is filling up the element
+// console.log(heading.innerHTML);
+
+// //shows the element and what is filling up the element as well as the white space
+// console.log(heading.outerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent)
+
+//this can be slow in the browser
+// pizzaList.textContent = `${pizzaList.textContent} 🍕`
+
+pizzaList.insertAdjacentText("beforeend", '🍕')
