9dca9fd51479e2a7a28c5b27c6a1da88ebbe692b
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-12-15 15:50:52 -0500

    Completed two more sections of the DOM

--- exercises/DOM/the-dom.js
+++ exercises/DOM/the-dom.js
@@ -1,29 +1,63 @@
-const p = document.querySelectorAll('p');
-console.log(p)
+// const p = document.querySelectorAll('p');
+// console.log(p)
 
-const imgs = document.querySelector('.item img');
-console.log(imgs);
+// const imgs = document.querySelector('.item img');
+// console.log(imgs);
 
-const heading = document.querySelector('h2');
-//.dir shows us the object properties - they be used as getters or setters
-// console.dir(heading)
+// const heading = document.querySelector('h2');
+// //.dir shows us the object properties - they be used as getters or setters
+// // console.dir(heading)
 
-// //example of a getter
-// console.dir(heading.textContent);
+// // //example of a getter
+// // console.dir(heading.textContent);
 
-// //example of a setter
-// heading.textContent = "This is the best!";
+// // //example of a setter
+// // heading.textContent = "This is the best!";
 
-//shows what is filling up the element
-// console.log(heading.innerHTML);
+// //shows what is filling up the element
+// // console.log(heading.innerHTML);
 
-// //shows the element and what is filling up the element as well as the white space
-// console.log(heading.outerHTML);
+// // //shows the element and what is filling up the element as well as the white space
+// // console.log(heading.outerHTML);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent)
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent)
 
-//this can be slow in the browser
-// pizzaList.textContent = `${pizzaList.textContent} 🍕`
+// //this can be slow in the browser
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`
 
-pizzaList.insertAdjacentText("beforeend", '🍕')
+// pizzaList.insertAdjacentText("beforeend", '🍕')
+
+//Classes!
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+// console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
+
+//setter
+pic.alt = 'Cute';
+//getter
+// console.log(pic.alt);
+
+pic.width = 200;
+// console.log(pic.naturalWidth);
+
+// window.addEventListener('load', function(){
+//     console.log(pic.naturalWidth);
+// })
+
+pic.setAttribute('alt', "Really cool")
+// console.log(pic.getAttribute('alt'))
+
+const custom = document.querySelector('.custom');
+console.log(custom.dataset)
+
+custom.addEventListener('click', function(){
+    alert(`Welcome ${custom.dataset.name}`)
+})
