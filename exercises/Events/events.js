9a94b569c669da1c850356b9e552d59e17627496
Jennifer Morales <jennifer.morales1201@gmail.com>
2021-01-04 16:01:55 -0500

    Completed videos 29 and 30

--- exercises/Events/events.js
+++ exercises/Events/events.js
@@ -14,10 +14,38 @@ buttonTwo.addEventListener('click', hooray);
 //Listen on multiple items
 const buyButtons = document.querySelectorAll('button.buy');
 
-function buyItem() {
-    console.log('Buying Item')
-}
+function handleBuyButtonClick(event){
+    const button = event.target;
+    // console.log('You are buying it');
+    // console.log(button.textContent);
+    // console.log(parseFloat(event.target.dataset.price));
+    console.log(event.target);
+    console.log(event.currentTarget);
+    console.log(event.target === event.currentTarget);
 
-buyButtons.forEach(button => {
-    button.addEventListener('click', buyItem )
+
+    //If you click the strong tag, you not only clicked on the strong tag, but also the button,
+    //the body, the html tag, the browser, the operating system, etc.
+    //to stop this from happening use stopPropataion
+    //Stop this event from bubbling up
+    event.stopPropagation();
+}
+//event.target and event.currentTarget are the same unless you have elements nested inside
+//the event.target is the thing that actually got clicked
+//the event.currentTarget is the thing that fired the event listener
+
+buyButtons.forEach(buyButton => {
+    buyButton.addEventListener('click', handleBuyButtonClick);
+});
+
+window.addEventListener('click', function(event){
+    console.log('You clicked the window');
+    console.log(event.target);
+}, {capture: true});
+
+const photoElement = document.querySelector('.photo');
+
+photoElement.addEventListener('mousemove', function(e) {
+    console.log(e.currentTarget);
+    console.log(this);
 })
