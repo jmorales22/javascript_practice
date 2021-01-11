91067da9f8dc0cbf826e9e00c35b499d817fee66
Jennifer Morales <jennifer.morales1201@gmail.com>
2021-01-11 15:54:50 -0500

    Completed the etch-a-sketch exercise

---
+++ exercises/Etch-a-Sketch/etch-a-sketch.js
@@ -0,0 +1,78 @@
+//Select the elents on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+
+const ctx = canvas.getContext('2d');
+
+const shakeButton = document.querySelector('.shake');
+
+const MOVE_AMOUNT = 20;
+
+// Setup our canvas for drawing
+const { width, height } = canvas;
+
+//create random x and y starting points on the canvas
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+ctx.beginPath(); // start the drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
+// Write a draw function
+function draw({ key }) {
+  // Increment hue by one
+  hue += 10;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+  // start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // move our x and y values depending on what the user did
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+// Write a handler for the keys
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
+
+// Clear/Shake function
+function clearCanvas() {
+    canvas.classList.add('shake');
+    ctx.clearRect(0, 0, width, height);
+    canvas.addEventListener('animationend', function(){
+        canvas.classList.remove('shake');
+    }, {once: true})
+}
+
+//Listen for arrow keys
+window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
